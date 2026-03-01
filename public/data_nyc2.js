66e1e24566e7d0b9cc55a2afcf235300207160fb
Jordan <jordanbmcgowen@gmail.com>
2026-03-01 16:29:34 -0600

    data: add NYC part 2 restaurant data (100 records)

---
+++ public/data_nyc2.js
@@ -0,0 +1,2 @@
+/* DineScores New York data part 2 */
+window.DATA_NEW_YORK_2 = [{"n":"Great Taste Usa","a":"33 Catherine Street","c":"New York","s":"NY","z":"10038","lt":40.712431482985,"ln":-73.997024114289,"d":"20240101","g":2,"r":[["Food",90],["Service",85],["Atmosphere",80]],"i":"placeholder"}];
